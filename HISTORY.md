deafa19de8d078f1a6e196c74e2a566950eadcb3
Jarrett Widman <jarrett@24m2.com>
2015-09-11 10:59:31 -0500

    Process all mode: sections

---
+++ HISTORY.md
@@ -0,0 +1,8 @@
+# History
+
+#### 1.0.1 / 9-11-2015
+- Remove unused dependency xml2js
+- Process all `mode:` sections (updated test data)
+
+#### 1.0.0 / 9-10-2015
+- Initial version
--- source/index.js
+++ source/index.js
@@ -14,68 +14,77 @@ parse.parseContent = function ( text, cb )
         return cb( new Error( "No coverage found" ) );
     }
 
-    var lines = modes[ 1 ].replace( "\r\n", "\n" ).split( /[\n\r]/g );
-    lines = lines.slice( 1 ); // the first line is just the mode type
-
-    lines.forEach( function ( line )
+    modes.forEach( function ( mode )
     {
-        var parts = line.split( ":" );
-        if( !parts.length )
+        if( !mode.length )
         {
             return;
         }
 
-        var path = parts[ 0 ];
-        var values = parts[ 1 ];
+        var lines = mode.replace( "\r\n", "\n" ).split( /[\n\r]/g );
+        lines = lines.slice( 1 ); // the first line is just the mode type
 
-        if( !path || !values )
+        lines.forEach( function ( line )
         {
-            return;
-        }
-
-        if( !files[ files.length - 1 ] || files[ files.length - 1 ].file !== path )
-        {
-            var name = path.split( "/" );
-            name = name[ name.length - 1 ];
-
-            files.push( {
-                title: name,
-                file: path,
-                lines: {
-                    found: 0,
-                    hit: 0,
-                    details: []
-                }
-            } );
-        }
-
-        var file = files[ files.length - 1 ];
-
-        var startLine = Number( values.split( "," )[ 0 ].split( "." )[ 0 ] );
-        var endLine = Number( values.split( "," )[ 1 ].split( "." )[ 0 ] );
-        var hit = Number( values.split( " " )[ 2 ] );
-
-        file.lines.found += endLine - startLine + 1;
-        for( var lineNumber = startLine; lineNumber <= endLine; lineNumber++ )
-        {
-            var existingLine = file.lines.details.filter( function ( ex )
+            var parts = line.split( ":" );
+            if( !parts.length )
             {
-                return ex.line === lineNumber;
-            } )[ 0 ];
-
-            if( existingLine )
-            {
-                existingLine.hit += hit;
+                return;
             }
-            else
+
+            var path = parts[ 0 ];
+            var values = parts[ 1 ];
+
+            if( !path || !values )
             {
-                file.lines.details.push( {
-                    line: lineNumber,
-                    hit: hit
+                return;
+            }
+
+            if( !files[ files.length - 1 ] || files[ files.length - 1 ].file !== path )
+            {
+                var name = path.split( "/" );
+                name = name[ name.length - 1 ];
+
+                files.push( {
+                    title: name,
+                    file: path,
+                    lines: {
+                        found: 0,
+                        hit: 0,
+                        details: []
+                    }
                 } );
             }
-        }
+
+            var file = files[ files.length - 1 ];
+
+            var startLine = Number( values.split( "," )[ 0 ].split( "." )[ 0 ] );
+            var endLine = Number( values.split( "," )[ 1 ].split( "." )[ 0 ] );
+            var hit = Number( values.split( " " )[ 2 ] );
+
+            file.lines.found += endLine - startLine + 1;
+            for( var lineNumber = startLine; lineNumber <= endLine; lineNumber++ )
+            {
+                var existingLine = file.lines.details.filter( function ( ex )
+                {
+                    return ex.line === lineNumber;
+                } )[ 0 ];
+
+                if( existingLine )
+                {
+                    existingLine.hit += hit;
+                }
+                else
+                {
+                    file.lines.details.push( {
+                        line: lineNumber,
+                        hit: hit
+                    } );
+                }
+            }
+        } );
     } );
+
 
     files.forEach( function ( file )
     {
